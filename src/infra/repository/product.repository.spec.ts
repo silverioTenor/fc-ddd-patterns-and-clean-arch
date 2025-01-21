b2cd87cc76097e57ef3be9fc46f679b67570e0cd
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-01-21 12:23:58 -0300

    Product repository implementation

--- src/infra/repository/product.repository.spec.ts
+++ src/infra/repository/product.repository.spec.ts
@@ -1,4 +1,9 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize } from 'sequelize-typescript';
+import { v4 as uuid } from 'uuid';
+import ProductModel from '../db/sequelize/model/product.model';
+import Product from '../../domain/entity/product';
+import ProductRepository from './product.repository';
+import { where } from 'sequelize';
 
 describe('Product repository unit test', () => {
    let sequelize: Sequelize;
@@ -10,9 +15,68 @@ describe('Product repository unit test', () => {
          logging: false,
          sync: { force: true },
       });
+
+      sequelize.addModels([ProductModel]);
+      await sequelize.sync();
    });
 
    afterEach(async () => {
       await sequelize.close();
+   });
+
+   it('should create a product', async () => {
+      const productRepository = new ProductRepository();
+      const product = new Product(uuid(), 'Product 1', 100);
+
+      await productRepository.create(product);
+      const foundProduct = await productRepository.find(product.id);
+
+      expect(foundProduct).not.toBeNull();
+      expect(foundProduct).not.toEqual({});
+      expect(foundProduct).toStrictEqual(product);
+   });
+
+   it('should update a product', async () => {
+      const productRepository = new ProductRepository();
+      const product = new Product(uuid(), 'Product 1', 100);
+
+      await productRepository.create(product);
+
+      const updateProduct = new Product(product.id, 'Product 2', 200);
+      await productRepository.update(updateProduct);
+
+      const foundProduct = await productRepository.find(product.id);
+
+      expect(foundProduct).not.toBeNull();
+      expect(foundProduct).not.toEqual({});
+      expect(foundProduct).toStrictEqual(updateProduct);
+   });
+
+   it('should find a product', async () => {
+      const productRepository = new ProductRepository();
+      const product = new Product(uuid(), 'Product 1', 100);
+
+      await productRepository.create(product);
+      const foundProduct = await productRepository.find(product.id);
+
+      expect(foundProduct).not.toBeNull();
+      expect(foundProduct).not.toEqual({});
+      expect(foundProduct).toStrictEqual(product);
+   });
+
+   it('should find all products', async () => {
+      const productRepository = new ProductRepository();
+      const product1 = new Product(uuid(), 'Product 1', 100);
+      const product2 = new Product(uuid(), 'Product 2', 200);
+
+      await productRepository.create(product1);
+      await productRepository.create(product2);
+
+      const foundProducts = await productRepository.findAll();
+      const productList = [product1, product2];
+
+      expect(foundProducts).not.toBeNull();
+      expect(foundProducts).not.toEqual([]);
+      expect(foundProducts).toStrictEqual(productList);
    });
 });
