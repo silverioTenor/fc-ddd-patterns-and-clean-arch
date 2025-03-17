0adfef4bcc14ab4fbe570f768f57767a17cb6fc7
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-03-17 09:46:11 -0300

    Removed validation in order factory

--- src/domain/checkout/factory/order-factory.ts
+++ src/domain/checkout/factory/order-factory.ts
@@ -1,4 +1,4 @@
-import { IFactory } from '../../@shared/factory/factory.interface';
+import { IFactory } from '@domain/@shared/factory/factory.interface';
 import Order from '../entity/order';
 import OrderItem from '../entity/order-item';
 import { IOrderPayload } from './order-factory.interface';
@@ -6,16 +6,13 @@ import { IOrderPayload } from './order-factory.interface';
 export default class OrderFactory implements IFactory<Order> {
    create(payload: IOrderPayload): Order {
       const orderItems = payload.products.map((product: any, i: number) => {
-         if (payload?.orderItemId) {
-            return new OrderItem(
-               product.id,
-               product.name,
-               product.quantity,
-               product.price,
-               payload.orderItemId[i],
-            );
-         }
-         return new OrderItem(product.id, product.name, product.quantity, product.price);
+         return new OrderItem(
+            product.id,
+            product.name,
+            product.quantity,
+            product.price,
+            payload.orderItemId ? payload.orderItemId[i] : undefined,
+         );
       });
       const order = new Order(payload.customerId, orderItems, payload?.orderId);
 
