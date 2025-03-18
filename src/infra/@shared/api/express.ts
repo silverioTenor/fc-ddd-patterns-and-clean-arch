ef51749d603755d1928cbba2a9d1878581d17fca
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-03-18 16:26:57 -0300

    Implementing documentation with swagger

--- src/infra/@shared/api/express.ts
+++ src/infra/@shared/api/express.ts
@@ -8,10 +8,27 @@ import { Request, Response } from 'express';
 import ErrorHandling from './middleware/error.handling.midlleware';
 import OrderModel from '../../order/db/sequelize/model/order.model';
 import OrderItemModel from '../../order/db/sequelize/model/order-item.model';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yaml';
+import fs from 'fs';
+import path from 'path';
 
 export const app = express();
 app.use(express.json());
 app.use(routes);
+
+const swaggerDocConverted = fs.readFileSync(
+   path.join(__dirname, '..', 'config', 'swagger.yml'),
+   'utf8',
+);
+const swaggerDocFile = YAML.parse(swaggerDocConverted);
+// var options = {
+//    swaggerOptions: {
+//       url: 'http://petstore.swagger.io/v2/swagger.json',
+//    },
+// };
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocFile));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
    return ErrorHandling.handle(err, req, res, next) as any;
